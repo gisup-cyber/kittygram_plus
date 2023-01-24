bfb475d30d4a341bbbbe70ab39ffd009ceb19e4f
Игорь Васильев <assists1@yandex.ru>
2023-01-24 20:43:37 +0300

    Add auth sistem

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
@@ -9,5 +10,7 @@ router.register(r'owners', OwnerViewSet)
 router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
+    path('api-token-auth/', views.obtain_auth_token),
     path('', include(router.urls)),
+    
 ] 
