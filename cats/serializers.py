90bd2f4ea49ec1a59cc6488f0ee1a3e757d13815
Игорь Васильев <assists1@yandex.ru>
2023-01-24 13:18:47 +0300

    Add age cats calculation

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from rest_framework import serializers
 
 from .models import Achievement, AchievementCat, Cat, Owner
@@ -13,10 +15,14 @@ class AchievementSerializer(serializers.ModelSerializer):
 class CatSerializer(serializers.ModelSerializer):
 
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         if 'achievements' not in self.initial_data:
